47c8e25545e22ea3574da2c077dca7bc27c590c9
Nahari Rasif <naharirasif18@gmail.com>
2023-05-23 17:52:33 +0700

    Init second project

---
+++ Project_02/init_first_app.py
@@ -0,0 +1,64 @@
+import uvicorn
+
+from typing import Optional
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+class BookRequest(BaseModel): #pydentic, biar ada data validation (biar ga semena mena ngisi data)
+    id: Optional[int] = Field(title='ID is not needed')
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6) #gt artinya greater than, lt artinya lower than
+
+    class Config: #Config supaya lebih jelas deskripsinya
+        schema_extra = {
+            'example': {
+                'title': 'A new book',
+                'author': 'codingwithrasif',
+                'description': 'A new description of a book',
+                'rating': 5
+            }
+        }
+
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+]
+
+@app.get('/books')
+async def read_all_books():
+    return BOOKS
+
+@app.post('/create-book')
+async def create_book(format_buku: BookRequest):
+    new_book = Book(**format_buku.dict())
+    BOOKS.append(find_book_id(new_book))
+
+# biar id nya otomatis increment walaupun ga dimasukin input
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8010)
